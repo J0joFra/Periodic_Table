fb9ac633d936eabc67f868aba22b8e61af2ef59d
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-18 18:30:15 +0200

    Check Composto Binario

--- nomenclatura.py
+++ nomenclatura.py
@@ -4,6 +4,8 @@ import pandas as pd
 # Funzione che analizza il composto ricercato
 def analisi_composto(composto):
     divisione = composto.split(" ")
+    if len(divisione) == 3:
+        print("Possibile binario")
     print(divisione)
     return divisione
 
