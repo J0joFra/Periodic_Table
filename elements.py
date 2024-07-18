9c73f545b19055ddcc8274c09ae82d33591a7dc3
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-18 12:21:33 +0200

    MongoDB connection

---
+++ elements.py
@@ -0,0 +1,13 @@
+import pymongo
+import pandas as pd
+
+# Connessione a MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# Creazione o selezione del database
+db = client["tavola_periodica"]
+
+# Creazione o selezione della collezione per gli elementi chimici
+collezione_elementi = db["elementi"]
+
+
