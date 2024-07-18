5edc8775ac06a4faba5bc500b8cab1a814b1d283
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-18 18:25:45 +0200

    Split Composto

--- nomenclatura.py
+++ nomenclatura.py
@@ -1,1 +1,14 @@
+import pymongo
+import pandas as pd
+
+# Funzione che analizza il composto ricercato
+def analisi_composto(composto):
+    divisione = composto.split(" ")
+    print(divisione)
+    return divisione
+
+# Composto da ricercare
+composto = input("Inserisci il nome di un composto: ")
+
+analisi_composto(composto)
 
