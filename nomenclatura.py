06f069ffb640159598312aeebee08b60457e0ed7
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-18 18:41:35 +0200

    Composti Binari

--- nomenclatura.py
+++ nomenclatura.py
@@ -8,10 +8,14 @@ def analisi_composto(composto):
     # Controllo se è un composto binario
     if len(divisione) <= 3: 
         if divisione[0].endswith("uro"):
-            print("Composto binario")
-    print(divisione)
+            print(f"il {composto} è un composto binario perchè ha il suffisso -uro")
+            composto_binario(divisione)
     return divisione
 
+# Funzione per i composti binari
+def composto_binario(divisione):
+    print("Ciao")
+    
 # Composto da ricercare
 composto = input("Inserisci il nome di un composto: ")
 
