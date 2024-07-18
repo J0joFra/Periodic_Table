61ec5adb6a59e01b9dba8c5b3ab18b9eaad5f2a5
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-18 12:27:59 +0200

    Added elements

--- elements.py
+++ elements.py
@@ -2,7 +2,7 @@ import pymongo
 import pandas as pd
 
 # Connessione a MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb+srv://jofrancalanci:Cf8m2xsQdZgll1hz@element.2o7dxct.mongodb.net/")
 
 # Creazione o selezione del database
 db = client["tavola_periodica"]
@@ -10,6 +10,12 @@ db = client["tavola_periodica"]
 # Creazione o selezione della collezione per gli elementi chimici
 collezione_elementi = db["elementi"]
 
-# Lettura dei dati dal CSV con Pandas (esempio)
-df = pd.read_csv('Data\elemtents.csv')
+# Lettura dei dati dal CSV
+df = pd.read_csv('Data/elements.csv')
 
+# Inserimento dei dati nella collezione MongoDB
+elementi_da_inserire = df.to_dict(orient='records')
+collezione_elementi.insert_many(elementi_da_inserire)
+
+# Chiusura della connessione a MongoDB
+client.close()
