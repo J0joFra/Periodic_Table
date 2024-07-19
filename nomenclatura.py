43e3b9fcd33eafa12f912d3f4638e88d2814b025
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-19 09:35:35 +0200

    Idruri Metallici

--- nomenclatura.py
+++ nomenclatura.py
@@ -31,26 +31,28 @@ def composto_binario(divisione, collezione_elementi):
     # Controlla se è un "Idruro"
     if divisione[0].startswith('idr'):
         el_2 = 'Idrogeno'
-        formatta_idruro(divisione, collezione_elementi)
+        formatta_idruro(el_1, collezione_elementi)
     # Controlla se è un "Solfuro"
-    if divisione[0].startswith('solf'):
+    elif divisione[0].startswith('solf'):
         el_2 = 'Zolfo'
     
-    el_parziale = divisione[0].removesuffix('uro')
-    el = el_parziale.capitalize()
-    # Secondo elemento
-    el_2 = cerca_elemento(el, collezione_elementi)
-    
-    print(f"Il {composto} è composto da {el_2} + {el_1}")
-    
-    # Costruzione della formula
-    simbolo_el_1 = cerca_simbolo(el_1.capitalize(), collezione_elementi)
-    simbolo_el_2 = cerca_simbolo(el_2.capitalize(), collezione_elementi)
-    
-    if simbolo_el_1 and simbolo_el_2:
-        print(f"{simbolo_el_1}{simbolo_el_2}")
     else:
-        print(f"Simbolo non trovato per uno o entrambi gli elementi")
+        el_parziale = divisione[0].removesuffix('uro')
+        el = el_parziale.capitalize()
+        # Secondo elemento
+        el_2 = cerca_elemento(el, collezione_elementi)
+    
+    if el_2:
+        print(f"Il {composto} è composto da {el_2} + {el_1}")
+    
+        # Costruzione della formula
+        simbolo_el_1 = cerca_simbolo(el_1.capitalize(), collezione_elementi)
+        simbolo_el_2 = cerca_simbolo(el_2.capitalize(), collezione_elementi)
+    
+        if simbolo_el_1 and simbolo_el_2:
+            print(f"{simbolo_el_1}{simbolo_el_2}")
+        else:
+            print(f"Simbolo non trovato per uno o entrambi gli elementi")
 
 # Funzione per cercare il nome completo di un elemento  
 def cerca_elemento(el, collezione_elementi):
@@ -83,18 +85,26 @@ def cerca_simbolo(nome, collezione_elementi):
 
 # Funzione per determinare gli idruri
 def formatta_idruro(el_1, collezione_elementi):
-    # Controlla il gruppo di el_1
     elementi = collezione_elementi.find()
     for elemento in elementi:
+        # Recupera il nome dell'elemento
+        nome_elemento = elemento.get("name_italian", "")
         # Recupera il gruppo dell'elemento
         gruppo_elemento = elemento.get("group_id", "")
         # Recupera il simbolo dell'elemento
         simbolo_elemento = elemento.get("symbol", "")
         
-        # Controlla se il nome dell'elemento corrisponde al nome dato
-        if nome_elemento.lower() == nome.lower():
-            return simbolo_elemento
-    return
+        if nome_elemento.lower() == el_1.lower():
+            if gruppo_elemento in [1, 2] and nome_elemento not in ['Litio', 'Berillio']:
+                # Idruri metallici
+                formula = f"{simbolo_elemento}H"
+                print(f"Idruro metallico: {formula}")
+            else:
+                # Idruri covalenti
+                formula = f"H{simbolo_elemento}"
+                print(f"Idruro covalente: {formula}")
+            return formula
+    return None
 
 # URI di connessione a MongoDB
 mongo_uri = "mongodb+srv://jofrancalanci:Cf8m2xsQdZgll1hz@element.2o7dxct.mongodb.net/"
